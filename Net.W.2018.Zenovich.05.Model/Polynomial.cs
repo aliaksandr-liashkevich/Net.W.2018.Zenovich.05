b40c662b3b41354ef984c6e67aa89c2828e95309
azenovich <alexander.zenovich@gmail.com>
2018-03-22 19:35:27 +0300

    Polynomial was changed.

--- Net.W.2018.Zenovich.05.Model/Polynomial.cs
+++ Net.W.2018.Zenovich.05.Model/Polynomial.cs
@@ -24,6 +24,8 @@ namespace Net.W._2018.Zenovich._05.Model
         private static Polynomial FactoryInitialization(Polynomial first, Polynomial second, Func<double, double, double> filter)
         {
             int minLength = GetMinLength(first, second);
+            int maxLength = first._coefficients.Length != minLength ? minLength : second._coefficients.Length;
+
             double[] firstCoefficients = first._coefficients;
             double[] secondCoefficients = second._coefficients;
 
@@ -34,9 +36,70 @@ namespace Net.W._2018.Zenovich._05.Model
                 result[i] = filter(firstCoefficients[i], secondCoefficients[i]);
             }
 
+            firstCoefficients = firstCoefficients.Length == maxLength ? firstCoefficients : secondCoefficients;
+
+            for (int i = minLength; i < maxLength; i++)
+            {
+                result[i] = firstCoefficients[i];
+            }
+
             return new Polynomial(result);
         }
 
+        public static Polynomial operator +(Polynomial first, Polynomial second)
+        {
+            return FactoryInitialization(first, second, PlusFilter);
+        }
+
+        public static Polynomial operator -(Polynomial first, Polynomial second)
+        {
+
+            return FactoryInitialization(first, second, MinusFilter);
+        }
+
+        public static Polynomial operator *(Polynomial first, Polynomial second)
+        {
+
+            return FactoryInitialization(first, second, MultiplyFilter);
+        }
+
+        public static bool operator ==(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            return EqualityComparer<Polynomial>.Default.Equals(polynomial1, polynomial2);
+        }
+
+        public static bool operator !=(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            return !(polynomial1 == polynomial2);
+        }
+
+        private static double PlusFilter(double first, double second)
+        {
+            return first + second;
+        }
+
+        private static double MinusFilter(double first, double second)
+        {
+            return first - second;
+        }
+
+        private static double MultiplyFilter(double first, double second)
+        {
+            return first * second;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var polynomial = obj as Polynomial;
+            return polynomial != null &&
+                   EqualityComparer<double[]>.Default.Equals(_coefficients, polynomial._coefficients);
+        }
+
+        public override int GetHashCode()
+        {
+            return 710347640 + EqualityComparer<double[]>.Default.GetHashCode(_coefficients);
+        }
+        /*
         private static bool IsEquivalentPolynomial(Polynomial first, Polynomial second)
         {
             if (first._coefficients.Length != second._coefficients.Length)
@@ -57,54 +120,6 @@ namespace Net.W._2018.Zenovich._05.Model
             }
 
             return true;
-        }
-
-        public static Polynomial operator +(Polynomial first, Polynomial second)
-        {
-            return FactoryInitialization(first, second, (firstCoefficients, secondCoefficients) =>
-            {
-                return firstCoefficients + secondCoefficients;
-            });
-        }
-
-        public static Polynomial operator -(Polynomial first, Polynomial second)
-        {
-
-            return FactoryInitialization(first, second, (firstCoefficients, secondCoefficients) =>
-            {
-                return firstCoefficients - secondCoefficients;
-            });
-        }
-
-        public static Polynomial operator *(Polynomial first, Polynomial second)
-        {
-
-            return FactoryInitialization(first, second, (firstCoefficients, secondCoefficients) =>
-            {
-                return firstCoefficients * secondCoefficients;
-            });
-        }
-
-        public static bool operator ==(Polynomial first, Polynomial second)
-        {
-            return IsEquivalentPolynomial(first, second);
-        }
-
-        public static bool operator !=(Polynomial first, Polynomial second)
-        {
-            return !(first == second);
-        }
-
-        public override bool Equals(object obj)
-        {
-            var polynomial = obj as Polynomial;
-            return polynomial != null &&
-                   EqualityComparer<double[]>.Default.Equals(_coefficients, polynomial._coefficients);
-        }
-
-        public override int GetHashCode()
-        {
-            return 1112971371 + EqualityComparer<double[]>.Default.GetHashCode(_coefficients);
-        }
+        }*/
     }
 }
